1711dc3fbe92aa1c7ee7555f734e24c5ff7a9182
rahatkabir007 <rahatkabir04@gmail.com>
2023-05-28 17:10:45 +0600

    new code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3001);
-  console.log(`PREMIUM APK CODE PDF NEWSPAPER HUSTLE SERVER listening on port ${process.env.PORT}`);
+  await app.listen(8000 || 3001);
+  console.log(`PREMIUM APK CODE PDF NEWSPAPER HUSTLE SERVER listening on port 8000`);
 }
 bootstrap();
